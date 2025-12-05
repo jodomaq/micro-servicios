29ec735afc57dd1aadc258962ea459ff3c640a4f
Donato Maldonado <jodomaq@gmail.com>
2025-12-04 22:11:11 -0600

    pak

--- ExcelConverter/vite.config.js
+++ ExcelConverter/vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ExcelConverter/',
   server: {
     port: 5174
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
